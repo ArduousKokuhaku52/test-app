4092f831f266619532ad2a62413b52e76420279c
ArduousKokuhaku52 <arduouskokuhaku.gg@protonmail.com>
2023-06-05 19:54:58 -0500

    Added InteractionBar to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import SideBar from "./components/SideBar/SideBar";
+import InteractionBar from "./components/InteractionBar/InteractionBar";
 import MainBody from "./components/Main/MainBody";
 
 function App() {
   return (
     <div className="app-main">
       <MainBody />
+      <InteractionBar />
       <SideBar />
     </div>
   );
