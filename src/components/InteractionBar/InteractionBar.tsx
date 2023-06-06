521a6d5904e05a5e35d702ee76daf447ad6160f2
ArduousKokuhaku52 <arduouskokuhaku.gg@protonmail.com>
2023-06-05 21:00:58 -0500

    Added placeholder boxes to represent the interaction list

--- src/components/InteractionBar/InteractionBar.tsx
+++ src/components/InteractionBar/InteractionBar.tsx
@@ -1,5 +1,37 @@
+import InteractionBarFriendBox from "./InteractionBarFriendBox";
+
 const InteractionBar = () => {
-  return <div className="app-interactionbar-body"></div>;
+  return (
+    <div className="app-interactionbar-body">
+      <InteractionBarFriendBox name="Friends" />
+      <InteractionBarFriendBox name="Message Requests" />
+      <InteractionBarFriendBox name="DIRECT MESSAGES" />
+      <InteractionBarFriendBox name="umster" />
+      <InteractionBarFriendBox name="korkor" />
+      <InteractionBarFriendBox name="hexor" />
+      <InteractionBarFriendBox name="devkoko" />
+      <InteractionBarFriendBox name="tacopaco" />
+      <InteractionBarFriendBox name="breacher1337" />
+      <InteractionBarFriendBox name="ink" />
+      <InteractionBarFriendBox name="trio" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+      <InteractionBarFriendBox name="placeholder" />
+    </div>
+  );
 };
 
 export default InteractionBar;
